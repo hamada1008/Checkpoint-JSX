bc3d24f922afdfc61a6f8c9478be849d29258a39
hamada1008 <86958719+hamada1008@users.noreply.github.com>
2021-08-04 18:36:09 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import "./styles.css";
+import im from "./imageInSrc.jpg";
+import vid from "./myVideo.mp4";
+
+export default function App() {
+  return (
+    <div className="App">
+      <div style={{ border: "solid 1px black", maxWidth: "100vw" }}>
+        <h1 className="title red">Your name here</h1>
+
+        <img src={im} alt="source" />
+
+        <img src="/imageInPublic.jpg" alt="public" />
+      </div>
+
+      <video width={320} height={240} controls>
+        <source src={vid} type="video/mp4" />
+      </video>
+    </div>
+  );
+}
